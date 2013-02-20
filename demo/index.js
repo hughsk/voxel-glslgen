97bbcfb2b67ecb4549bba521753cd66525f65156
Hugh Kennedy <hughskennedy@gmail.com>
2013-02-21 00:09:11 +1100

    Nicening up the demo

--- demo/index.js
+++ demo/index.js
@@ -18,27 +18,33 @@ var game = window.game = createGame({
     , 'vec2 F2 = cellular(pos / 31.276);'
     , 'float n1 = F1.y-F1.x;'
     , 'float n2 = F2.y-F2.x;'
-    , 'voxelBlock((n2-n1 > 0.0025) ? 0.0 : 1.0 + n2 * 4.0);'
+    , 'voxelBlock(n2-n1 < max(pos.y * 0.025, 0.0) ? 0.0 : 2.0 + n2 * 4.0);'
   , '}'
 
   ].join('\n')),
   chunkDistance: 2,
   materials: [
+    'dirt',
     ['grass', 'dirt', 'grass_dirt'],
-    ['whitewool', 'bedrock', 'cobblestone'],
-    ['bedrock', 'cobblestone'],
-    ['glowstone'],
-    'grass',
-    'plank'
+    ['whitewool', 'cobblestone', 'cobblestone'],
+    'a1-clay-grey',
+    'lampon-redstone',
+    'glowstone'
   ],
   texturePath: 'textures/',
   worldOrigin: [0, 0, 0],
   controls: { discreteFire: true },
-  chunkSize: 30,
+  chunkSize: 32,
+  skyColor: 0xffffff
 })
 
 game.appendTo(document.getElementById('container'))
-game.scene.fog = new game.THREE.FogExp2( game.skyColor, 0.001 );
+game.scene.fog = new game.THREE.FogExp2( game.skyColor, 0.0015 );
+
+var pp = require('voxel-pp')(game)
+var grading = require('voxel-colorist')(pp, {
+  table: 'table.png'
+})
 
 // create the player from a minecraft skin file and tell the
 // game to use it as the main player
